2dfbbb7fcdf9da037af9d804c90a2c491e13110c
hb2006 <hb2006>
2017-02-11 18:03:33 +0900

    17-02-11 15:30

--- app/src/main/java/com/hanbit/app/contactapp/dao/MemberDao.java
+++ app/src/main/java/com/hanbit/app/contactapp/dao/MemberDao.java
@@ -1,26 +1,52 @@
 package com.hanbit.app.contactapp.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import com.hanbit.app.contactapp.domain.MemberBean;
 
 import java.util.ArrayList;
-
 /**
  * Created by hanbit on 2017-02-11.
  */
-public class MemberDao {
+public class MemberDao extends SQLiteOpenHelper {
+
+
+    public MemberDao(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = String.format("%s","CREATE TABLE Member( " +
+                "id text(10) PRIMARY KEY, " +
+                "password text(10), " +
+                "name text(10), " +
+                "phone text(13), " +
+                "addr text(10), " +
+                "profile text(10) " +
+                ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
 
 
     //CREATE
     void add(MemberBean bean) {
 
         String sql = String.format("INSERT INTO Member " +
-                "(id, password, name, phone, addr, profile) " +
-                "VALUES('%s','%s','%s','%s','%s','%s')"
+                        "(id, password, name, phone, addr, profile) " +
+                        "VALUES('%s','%s','%s','%s','%s','%s')"
                 , bean.getId(), bean.getPassword(), bean.getName(), bean.getPhone(), bean.getAddr(), bean.getProfile()
         );
-
-
-
 
 
     }
@@ -58,11 +84,22 @@ public class MemberDao {
 
     //UPDATE
     void update(MemberBean bean) {
+
+        String sql = String.format("UPDATE Member " +
+                " SET pass = '%s'" +
+                    ",phone = '%s' " +
+                    ",addr = '%s' " +
+                    ",profile = '%s' " +
+                " WHERE id='%s'",
+                bean.getPassword(), bean.getPhone(), bean.getAddr(),bean.getProfile()
+                );
     }
 
     //DELETE
     void delete(MemberBean bean) {
-    }
 
+        String sql = String.format("DELETE FROM MEMBER WHERE id ='%s", bean.getId());
+
+    }
 
 }
