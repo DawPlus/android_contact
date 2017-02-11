268e206dfa875d97e89ef46a6e767cd8f0085551
hb2006 <hb2006>
2017-02-11 12:10:32 +0900

    17-02-11 12:00

---
+++ app/src/main/java/com/hanbit/app/contactapp/presentation/MemberActivity.java
@@ -0,0 +1,73 @@
+package com.hanbit.app.contactapp.presentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.hanbit.app.contactapp.R;
+import com.hanbit.app.contactapp.domain.MemberBean;
+import com.hanbit.app.contactapp.service.MemberService;
+import com.hanbit.app.contactapp.service.MemberServiceImpl;
+
+public class MemberActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btAdd, btFindById, btFindByName, btList, btUpdate, btDelte;
+    MemberBean member;
+    MemberService service;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+
+        btAdd = (Button) findViewById(R.id.btAdd);
+        btFindById = (Button) findViewById(R.id.btFindById);
+        btFindByName = (Button) findViewById(R.id.btFindByName);
+        btList = (Button) findViewById(R.id.btList);
+        btUpdate = (Button) findViewById(R.id.btUpdate);
+        btDelte = (Button) findViewById(R.id.btDelte);
+
+        btAdd.setOnClickListener(this);
+        btFindById.setOnClickListener(this);
+        btFindByName.setOnClickListener(this);
+        btList.setOnClickListener(this);
+        btUpdate.setOnClickListener(this);
+        btDelte.setOnClickListener(this);
+
+        member = new MemberBean();
+        service = new MemberServiceImpl();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.btAdd:
+                service.add(member);
+                break;
+            case R.id.btFindById:
+                service.findOne(member);
+                break;
+            case R.id.btFindByName:
+                service.findSome("");
+                break;
+            case R.id.btList:
+                service.list();
+                break;
+            case R.id.btUpdate:
+                service.update(member);
+                break;
+            case R.id.btDelte:
+                service.delete(member);
+                break;
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/hanbit/app/contactapp/service/MemberService.java
@@ -0,0 +1,28 @@
+package com.hanbit.app.contactapp.service;
+
+import com.hanbit.app.contactapp.domain.MemberBean;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hanbit on 2017-02-04.
+ */
+public interface MemberService {
+
+    //CREATE
+    void add(MemberBean bean);
+
+    //READ ONE
+    MemberBean findOne(MemberBean bean);
+    //READ SOME
+    ArrayList<MemberBean> findSome(String keyword);
+    //READ ALL
+    ArrayList<MemberBean> list();
+
+    //UPDATE
+    void update(MemberBean bean);
+    //DELETE
+    void delete(MemberBean bean);
+
+
+}
